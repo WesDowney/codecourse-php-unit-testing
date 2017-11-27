da997f918657f7cfcae72a870a32e56dd3a9b144
Wes <wesdowney@gmail.com>
2017-11-26 20:54:56 -0500

    Added User class and some related tests

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,52 @@
+<?php
+
+class UserTest extends \PHPUnit\Framework\TestCase
+{
+	public function testThatWeCanGetTheFirstName()
+	{
+		$user = new \App\Models\User;
+
+		$user->setFirstName('Billy');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+	}	
+
+	public function testThatWeCanGetTheLastName()
+	{
+		$user = new \App\Models\User;
+
+		$user->setLastName('Smith');
+
+		$this->assertEquals($user->getLastName(), 'Smith');
+	}	
+
+	public function testFullNameIsReturned()
+	{
+		$user = new \App\Models\User;
+
+		$user->setFirstName('Billy');
+		$user->setLastName('Smith');
+
+		$this->assertEquals($user->getFullName(), 'Billy Smith');
+	}
+
+	public function testFirstAndLastNameAreTrimmed()
+	{
+		$user = new \App\Models\User;
+
+		$user->setFirstName(' Billy   ');
+		$user->setLastName('   Smith     ');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+		$this->assertEquals($user->getLastName(), 'Smith');
+	}
+
+	/*public function TestEmailAddressCanBeSet()
+	{
+		$user = new \App\Models\User;
+
+		$user->setEmail('billy@codecourse.com');
+
+		//$this->assertEquals($user->getEmail(), 'billy@codecourse.com');
+	}*/
+} 
